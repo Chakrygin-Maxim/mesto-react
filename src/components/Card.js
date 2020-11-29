fb9b4b6c10f4ab37425560c0821469385fdcc663
Chakrygin Maxim <m.chakrygin@gmail.com>
2020-11-29 22:43:23 +0300

    refactor: изменить class на className

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,13 @@
 function Card(props) {
   return (
-    <article class="element">
-      <img class="element__photo" src={props.link} alt={props.name} />
-      <button class="element__button-trash"></button>
-      <div class="element__info">
-        <h2 class="element__info-name">{props.name}</h2>
-        <div class="element__like-container">
-          <button class="element__button-like" type="button"></button>
-          <p class="element__likes">{props.likes.length}</p>
+    <article className="element">
+      <img className="element__photo" src={props.link} alt={props.name} />
+      <button className="element__button-trash"></button>
+      <div className="element__info">
+        <h2 className="element__info-name">{props.name}</h2>
+        <div className="element__like-container">
+          <button className="element__button-like" type="button"></button>
+          <p className="element__likes">{props.likes.length}</p>
         </div>
       </div>
     </article>
